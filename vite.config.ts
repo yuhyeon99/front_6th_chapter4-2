6272c194e6a3463a22b48165201f738cb621f0b8
yuhyeon99 <jujini31@gmail.com>
2025-09-11 23:56:28 +0900

    [deploy] 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: '/front_6th_chapter4-2/',
   }),
   defineTestConfig({
     test: {
